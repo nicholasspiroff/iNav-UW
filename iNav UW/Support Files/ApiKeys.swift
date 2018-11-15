182d8a1e469fb4b5195f5831a350319d34901634
Nick Spiroff <nicholasspiroff@gmail.com>
2018-11-14 19:36:20 -0600

    Changed API Key

--- iNav UW/Support Files/ApiKeys.swift
+++ iNav UW/Support Files/ApiKeys.swift
@@ -13,4 +13,4 @@ let kAPIKey = "e27cc0bd-8916-48a4-9ad8-538acafb16fd"
 let kAPISecret = "Yscj1dRXUzgezhZjbZB403ss1fYKOUtGSDUWWy6LUYQeBrbjApcq1Mic4txUsq7gSFAD19f4IEHkoToqgMvfGV6TIGH/kqXGsO5XcnASbPGTBt35+rqJ6PeVfTN1/w=="
 
 // Floor plan id is same as "FloorplanId" at the <http://developer.indooratlas.com/venues>
-let kFloorplanId = "4f9f1e10-d34e-4368-8b90-06f96c843457"
+let kFloorplanId = "fe1cf605-ba58-45c6-b529-f7ce22587f03"
